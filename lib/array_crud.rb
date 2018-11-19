33d19ee9e34b70f8db2c3012c6eb0615eecf82e9
Jesus Manny Baez <jesusmbaez@gmail.com>
2018-11-19 02:00:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  sailor_scouts.unshift("Sailor Neptune")
+  array.unshift("Sailor Neptune")
 end
 
 def add_element_to_start_of_array(array, element)
